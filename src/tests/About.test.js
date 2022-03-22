044f17bd81884560e6de8666a21a3525c00f5759
Felipe Fernandes <felipedfe@gmail.com>
2022-03-22 19:25:52 -0300

    requisito 4

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
 import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
 
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+const { screen } = require('@testing-library/react');
+
+test('Se a página contém um heading h2 com o texto `Page requested not found`', () => {
+  renderWithRouter(<NotFound />);
+  const titleNotFound = screen.getByRole('heading',
+    { level: 2, name: /Page requested not found/i });
+  expect(titleNotFound).toBeInTheDocument();
+});
+test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  renderWithRouter(<NotFound />);
+  const pikachuCrying = screen.getByAltText(/Pikachu crying because/i);
+  expect(pikachuCrying).toBeInTheDocument();
+  expect(pikachuCrying).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
