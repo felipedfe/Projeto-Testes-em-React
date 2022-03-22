142d8c9618ca60361bc573c54eb10b2302fae556
Felipe Fernandes <felipedfe@gmail.com>
2022-03-22 16:43:27 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+const { screen } = require('@testing-library/react');
+
+describe('Testando o componente <App.js />', () => {
+  test('Topo da aplicação deve conter um conjunto fixo de links de navegação.', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoritesLink).toBeInTheDocument();
+
+    // const numberOfLinks = screen.getAllByRole('link');
+    // expect(numberOfLinks).toHaveLength(3);
+  });
+
+  test('Se ao clicar em `Home` a aplicação vai para a página inicial', () => {
+    const { customHistory } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(homeLink);
+    expect(customHistory.location.pathname).toBe('/');
+  });
+  test('Se ao clicar em `About` a aplicação vai para a página de informações', () => {
+    const { customHistory } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    expect(customHistory.location.pathname).toBe('/about');
+  });
+  test('Se ao clicar em `Favorite Pokémons` o app vai para a página de favoritos', () => {
+    const { customHistory } = renderWithRouter(<App />);
+
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoritesLink);
+    expect(customHistory.location.pathname).toBe('/favorites');
+  });
+  test('Se a aplicação segue para a página NotFound em caso de URL inválida', () => {
+    const { customHistory } = renderWithRouter(<App />);
+
+    customHistory.push('/caminho-aleatorio');
+    const titleNotFound = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+});
---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+function renderWithRouter(component) {
+  const customHistory = createMemoryHistory();
+  const allSelectors = render(
+    <Router history={ customHistory }>
+      {component}
+    </Router>,
+  );
+
+  return { ...allSelectors, customHistory };
+}
+
+export default renderWithRouter;
